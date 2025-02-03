e6920141d86e868d416657801a250494313154cb
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2025-02-03 12:59:56 +0530

    date picker

--- src/components/home/Book.js
+++ src/components/home/Book.js
@@ -1,67 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Link } from "react-router-dom";
 
 export default function Book() {
+  // State for check-in and check-out dates
+  const [checkIn, setCheckIn] = useState(null);
+  const [checkOut, setCheckOut] = useState(null);
+
   return (
-    <>
-      <div
-        className="container-fluid booking pb-5 wow fadeIn"
-        data-wow-delay="0.1s"
-      > <div className="container">
-          <div className="bg-white shadow" style={{ padding: "35px" }}>
-            <div className="row g-2">
-              <div className="col-md-10">
-                <div className="row g-2">
+    <div className="container-fluid booking pb-5 wow fadeIn" data-wow-delay="0.1s">
+      <div className="container">
+        <div className="bg-white shadow" style={{ padding: "35px" }}>
+          <div className="row g-2">
+            <div className="col-md-10">
+              <div className="row g-2">
+                {/* Check-in Date Picker */}
                 <div className="col-md-3">
-      <div className="date" id="date1" data-target-input="nearest">
-        <input
-          type="text"
-          className="form-control datetimepicker-input"
-          placeholder="Check in"
-          data-target="#date1"
-          data-toggle="datetimepicker"
-        />
-      </div>
-    </div>
-    <div className="col-md-3">
-      <div className="date" id="date2" data-target-input="nearest">
-        <input
-          type="text"
-          className="form-control datetimepicker-input"
-          placeholder="Check out"
-          data-target="#date2"
-          data-toggle="datetimepicker"
-        />
-      </div>
-    </div>
-       
-                  <div className="col-md-3">
-                    <select className="form-select">
-                      <option selected>Adult</option>
-                      <option value="1">Adult 1</option>
-                      <option value="2">Adult 2</option>
-                      <option value="3">Adult 3</option>
-                      <option value="4">Adult 4</option>
-                      <option value="5">Adult 5</option>
-                      
-                    </select>
-                  </div>
-                  <div className="col-md-3">
-                    <select className="form-select">
-                      <option selected>Child</option>
-                      <option value="1">Child 1</option>
-                      <option value="2">Child 2</option>
-                      <option value="3">Child 3</option>
-                    </select>
-                  </div>
+                  <DatePicker
+                    selected={checkIn}
+                    onChange={(date) => setCheckIn(date)}
+                    className="form-control"
+                    placeholderText="Check-in"
+                    dateFormat="dd/MM/yyyy"
+                    minDate={new Date()} // Prevent past dates
+                  />
+                </div>
+
+                {/* Check-out Date Picker */}
+                <div className="col-md-3">
+                  <DatePicker
+                    selected={checkOut}
+                    onChange={(date) => setCheckOut(date)}
+                    className="form-control"
+                    placeholderText="Check-out"
+                    dateFormat="dd/MM/yyyy"
+                    minDate={checkIn || new Date()} // Ensure check-out is after check-in
+                  />
+                </div>
+
+                {/* Adults Dropdown */}
+                <div className="col-md-3">
+                  <select className="form-select">
+                    <option selected>Adult</option>
+                    <option value="1">1 Adult</option>
+                    <option value="2">2 Adults</option>
+                    <option value="3">3 Adults</option>
+                    <option value="4">4 Adults</option>
+                    <option value="5">5 Adults</option>
+                  </select>
+                </div>
+
+                {/* Children Dropdown */}
+                <div className="col-md-3">
+                  <select className="form-select">
+                    <option selected>Child</option>
+                    <option value="1">1 Child</option>
+                    <option value="2">2 Children</option>
+                    <option value="3">3 Children</option>
+                  </select>
                 </div>
               </div>
-              <div className="col-md-2">
-                <button className="btn btn-primary w-100">Submit</button>
-              </div>
+            </div>
+
+            {/* Submit Button */}
+            <div className="col-md-2">
+            <Link to="/rooms">
+              <button className="btn btn-primary w-100">Submit</button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
